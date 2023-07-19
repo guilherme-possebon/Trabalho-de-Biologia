4deeee76ab55481f9fe20a2b8ae5ca8f83f6bba6
Guilherme Possebon <gpossebon67@gmail.com>
2023-07-19 20:32:36 -0300

    Ajustando o "WhatIs"

--- src/Components/WhatIs.tsx
+++ src/Components/WhatIs.tsx
@@ -6,7 +6,6 @@ export default function WhatIs() {
   useEffect(() => {
     const observer = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
-        console.log(entry, 1111)
         if (entry.isIntersecting) {
           entry.target.classList.add('show')
         } else {
@@ -33,7 +32,7 @@ export default function WhatIs() {
         </div>
         {/* -------------------------Coluna 1------------------------- */}
 
-        <div className={styles.Coluna1Row2}>
+        <article className={styles.Coluna1Row2}>
           <h1 className="hidden text-center ">O que é Doença de Chagas?</h1>
           <p className="hidden p-answers">
             A Doença de Chagas é uma infecção parasitária transmitida pelo
@@ -42,52 +41,53 @@ export default function WhatIs() {
             mil casos novos por ano, 14.000 mortes/ano e 8.000 recém-nascidos
             infectados durante a gestação.
           </p>
-        </div>
+        </article>
 
-        <div className={styles.Coluna1Row6}>
+        <article className={styles.Coluna1Row6}>
           <h1 className="hidden text-center">Agente Etiológico</h1>
           <p className="hidden p-answers">
-            É o protozoário flagelado Trypanosoma cruzi. No sangue de
+            É o protozoário flagelado <em>Trypanosoma cruzi</em>. No sangue de
             vertebrados, apresenta-se na forma infectante tripomastigota e, nos
             tecidos, na forma amastigota. Já nos insetos, ocorrem várias
             maturações, destacando-se a forma infectante, que são as formas
             encontrada nas fezes e urina do inseto.
           </p>
-        </div>
-        <div className={styles.Coluna1Row10}>
+        </article>
+        <article className={styles.Coluna1Row10}>
           <h1 className="hidden text-center">
             Qual a forma evolutiva da doença de Chagas?
           </h1>
           <p className="hidden p-answers">
             Suas formas evolutivas são amastigota, tripomastigota e
-            epimastigota. Por exemplo, no sangue dos vertebrados, o T. cruzi se
-            apresenta sob a forma de tripomastigota, que é extremamente móvel,
-            e, nos tecidos, como amastigotas. No tubo digestivo dos vetores
-            ocorre a transformação do parasito, dando origem às formas
-            infectantes presentes nas fezes do inseto. A partir disso, é
-            possível entender a transmissão da DC e prevenir a doença.
+            epimastigota. Por exemplo, no sangue dos vertebrados, o{' '}
+            <em>T. cruzi</em> se apresenta sob a forma de tripomastigota, que é
+            extremamente móvel, e, nos tecidos, como amastigotas. No tubo
+            digestivo dos vetores ocorre a transformação do parasito, dando
+            origem às formas infectantes presentes nas fezes do inseto. A partir
+            disso, é possível entender a transmissão da Doença de Chagas e
+            prevenir a doença.
           </p>
-        </div>
+        </article>
 
         {/* -------------------------Coluna 2------------------------- */}
 
-        <div className={styles.Coluna2Row4}>
+        <article className={styles.Coluna2Row4}>
           <h1 className="hidden text-center">Vetores</h1>
           <p className="hidden p-answers">
-            Os vetores da tal doença são os triatomíneos Triatominae, mais
-            conhecido como “bicho-barbeiro”. São insetos da ordem Hemiptera,
-            como percevejos, cigarras, pulgões e etc. O Museu de Zoologia da USP
-            afirma que existem aproximadamente 89 mil espécies dessa ordem.
-            Existem inúmeras espécies de triatomíneos espalhados por todo país e
-            cada um tem um tamanho e cor diferente. Enquanto algumas espécies
-            variam entre 7 mm a 8 mm, algumas podem chegar até 38 mm. A cor pode
-            variar entre preto com listras laranjas, amarelo com listras pretas,
-            amarelo escuro entre outros. Afirma-se que existem mais 62 espécies
-            de triatomíneos no Brasil.
+            Os vetores da tal doença são os triatomíneos <em>Triatominae</em>,
+            mais conhecido como “bicho-barbeiro”. São insetos da ordem
+            Hemiptera, como percevejos, cigarras, pulgões e etc. O Museu de
+            Zoologia da USP afirma que existem aproximadamente 89 mil espécies
+            dessa ordem. Existem inúmeras espécies de triatomíneos espalhados
+            por todo país e cada um tem um tamanho e cor diferente. Enquanto
+            algumas espécies variam entre 7 mm a 8 mm, algumas podem chegar até
+            38 mm. A cor pode variar entre preto com listras laranjas, amarelo
+            com listras pretas, amarelo escuro entre outros. Afirma-se que
+            existem mais 62 espécies de triatomíneos no Brasil.
           </p>
-        </div>
+        </article>
 
-        <div className={styles.Coluna2Row8}>
+        <article className={styles.Coluna2Row8}>
           <h1 className="hidden text-center">Estado evolutivo</h1>
 
           <p className="hidden p-answers">
@@ -97,12 +97,12 @@ export default function WhatIs() {
             infectante, a via de transmissão e também a associação com outras
             patologias concomitantes.
           </p>
-        </div>
-        <div className={styles.Coluna2Row13}>
+        </article>
+        <article className={styles.Coluna2Row13}>
           <h1 className="hidden text-center">Ciclo de Vida</h1>
 
           <p className="hidden p-answers">
-            O ciclo de vida do T. cruzi inicia quando o barbeiro, ao se
+            O ciclo de vida do <em>T. cruzi</em> inicia quando o barbeiro, ao se
             alimentar do hospedeiro vertebrado, elimina suas fezes e urina, onde
             podem estar presentes as formas tripomastigotas. Os parasitas
             tripomastigotas penetram na pele e infectam as células do
@@ -116,7 +116,7 @@ export default function WhatIs() {
             para epimastigotas, onde multiplicam-se e tornam-se novamente
             tripomastigotas, as formas infectantes aos vertebrados.
           </p>
-        </div>
+        </article>
       </section>
     </section>
   )
